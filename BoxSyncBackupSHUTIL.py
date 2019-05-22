316cd0ba7e7815d642c7c67d42cd9b5ecb004279
cropleyc <cropleyc@onid.oregonstate.edu>
2019-05-22 13:39:50 -0700

    Create BoxSyncBackupSHUTIL.py

---
+++ BoxSyncBackupSHUTIL.py
@@ -0,0 +1,6 @@
+# shutil.copyfileobj(fsrc, fdst[, length])
+# shutil.copytree(src, dst, symlinks=False, ignore=None, copy_function=copy2, ignore_dangling_symlinks=False)
+
+import shutil
+
+shutil.copytree('c:\\Users\cropleyc\Documents\Manuals', 'c:\\Users\cropleyc\Box Sync\Box Sync\Manuals', symlinks=False, ignore=None, ignore_dangling_symlinks=False)
